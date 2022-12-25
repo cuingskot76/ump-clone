750f8d30b8f91db8aaa31131ecee817e844f8a2a
cuingskot76 <afrizapamujo@gmail.com>
2022-12-25 17:53:11 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <main className=" flex justify-center h-screen items-center flex-col gap-4">
+      <h1 className="text-gradient">React Tailwind Boilerplate</h1>
+      <p className="text-xl text-gray-500">
+        A simple boilerplate for React and Tailwind CSS
+      </p>
+      <p className="text-center ">
+        Made with <span className="text-red-600">❤</span> by{" "}
+        <a
+          href="https://github.com/cuingskot76"
+          className="text-gradient__2 font-bold"
+        >
+          Afrizal Setya Pamuji
+        </a>{" "}
+      </p>
+    </main>
+  );
+}
+
+export default App;
